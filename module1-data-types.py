4a3ef9e68b524b77b66bc169bf46c12dd4820ed1
heyfredlynch <fred@fredlynch.org>
2024-07-26 16:51:31 -0500

    1st time uploading homework...getting it right

---
+++ module1-data-types.py
@@ -0,0 +1,10 @@
+greeting = 'Hello, friend'  # A greeting message
+
+age = 35  # An integer representing the age
+
+speed1 = 4.5  # A floating-point number representing speed
+speed2 = 4.0  # A floating-point number representing speed
+speed3 = 4.  # A floating-point number representing speed (trailing dot is optional)
+speed4 = 4  # An integer, but will be interpreted as a float in calculations
+
+am_i_ugly = False  # A boolean value indicating beauty status
